4829f9e9507254c05b4a4d4ba24c470dadd7e386
Sukuna <isp-ramazanov69@mail.ru>
2023-07-30 23:50:57 +0300

    commit changes2

--- main.py
+++ main.py
@@ -38,44 +38,41 @@ with open('operations.json', "r", encoding="utf-8") as f:
     file_content = f.read()
     templates = json.loads(file_content)
 
-def filter_sort(templates):
-    # Фильтруем элементы с состоянием "EXECUTED" и сортируем по дате (от новых к старым)
+def filter_and_sort(templates):
     items_list = [item for item in templates if item.get("state") == "EXECUTED"]
-    items_list.sort(key=lambda x: get_date(x.get("date")), reverse=True)
-    # Берем первые пять элементов из отфильтрованного и отсортированного списка
-    items_list_last = items_list[:5]
-    return items_list_last
+    items_list.sort(key=lambda x: parse_date(x.get("date")), reverse=True)
+    last_items = items_list[:5]
+    return last_items
 
-def get_date(date_string):
-    # Функция для преобразования даты из строки в объект datetime
+def parse_date(date_str):
     try:
-        return datetime.strptime(date_string, "%d.%m.%Y")
+        return datetime.strptime(date_str, "%d.%m.%Y")
     except ValueError:
-        # Если дата не соответствует ожидаемому формату, пробуем преобразовать из формата ISO 8601
-        return datetime.fromisoformat(date_string).date()
+        return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
 
-def mask_card_number(card_number):
-    # Функция для замаскирования номера карты
-    visible_digits = 6
-    masked_portion = "*" * 2  # Количество маскированных символов между блоками видимых цифр
-    return f"{card_number[:visible_digits]} {masked_portion} **** {card_number[-4:]}"
-
-def prepare_message(items_list_last):
+def prepare_message(last_items):
     result = []
-    for item in items_list_last:
+    for item in last_items:
         description = item.get("description")
-        date = get_date(item.get('date')).strftime("%d.%m.%Y")
-        from_x = mask_card_number(item.get('from', ''))  # Замаскированный номер карты отправителя
-        to = mask_card_number(item.get('to', ''))  # Замаскированный номер карты получателя
-        operationAmount = item.get('operationAmount').get('amount')
-        operationAmount_1 = item.get('operationAmount').get('currency').get('name')
-        result.append(f"{date} {description}\n{from_x} -> Счет {to}\n{operationAmount} {operationAmount_1}")
+        date = item.get('date')
+        from_x = item.get('from', '')
+        to = item.get('to', '')
+        last_four_digits_from = from_x[-4:] if from_x else ''
+        last_four_digits_to = to[-4:] if to else ''
+        operation_amount = item.get('operationAmount').get('amount')
+        operation_currency = item.get('operationAmount').get('currency').get('name')
+        formatted_date = parse_date(date).strftime("%d.%m.%Y")
+        sender_info = f"Visa Platinum 7000 {from_x[-9:-5]}** **** {last_four_digits_from}" if from_x else "N/A"
+        receiver_info = f" -> Счет **{last_four_digits_to}" if to else " -> N/A"
+        result.append(f"{formatted_date} {description}\n{sender_info}{receiver_info}\n{operation_amount} {operation_currency}")
     return result
 
 # Пример использования:
-x = filter_sort(templates)
+x = filter_and_sort(templates)
 output = prepare_message(x)
 for line in output:
     print(line)
+
+
 
 
